2fca9f10d3225d27c7f416df3aa73e7f576525ac
Lisa Yip <imlisayip@gmail.com>
2022-10-31 12:15:22 -0700

    add heatmaps + award

--- src/data/data.js
+++ src/data/data.js
@@ -4,6 +4,7 @@ import Text from '../components/Text/Text'
 import Eyebrow from '../components/Eyebrow/Eyebrow'
 import Link from '../components/Link/Link'
 
+import HeatmapsExplore from '../data/fullstory-heatmaps.png'
 import JourneyMaps from '../data/fullstory-journey-maps.png'
 import COE from '../data/creativity-over-everything.png'
 import Product from '../data/patreon-product.png'
@@ -20,15 +21,24 @@ export const HEADER = {
 }
 export const PROJECTS = [
     {
+        title: 'FullStory Heatmaps',
+        description: "FullStory is a web-based digital intelligence system that helps optimize the client experience. Heatmaps provide cues for how users <strong>explore and navigate your website via visual, aggregate data</strong>. Worked closely with team to <strong>build ground work connecting APIs and visual pieces together using React, TypeScript</strong>, and Go.",
+        cta: 'Visit product page',
+        ctaLink: 'https://www.fullstory.com/platform/heatmaps/',
+        image: HeatmapsExplore,
+        alt: 'Screenshot of FullStory Heatmaps Product',
+        direction: true,
+    },
+    {
         title: 'FullStory Journey Maps',
-        description: "FullStory is a web-based digital intelligence system that helps optimize the client experience. Journey maps is a highly anticipated analysis for visualizing user paths. Worked closely with team to <strong>build and connect UI pieces together using React, TypeScript</strong>, and Go.",
-        cta: 'Visit site',
+        description: "FullStory is a web-based digital intelligence system that helps optimize the client experience. Journey maps is a <strong>highly anticipated analysis for visualizing user paths</strong>. Worked closely with team to <strong>build and connect UI pieces together using React, TypeScript</strong>, and Go.",
+        cta: 'Visit product page',
         ctaLink: 'https://www.fullstory.com/platform/journey-maps/',
         image: JourneyMaps,
         alt: 'Screenshot of FullStory Journey Maps Product',
     },
     {
-        title: 'Creativity Over Everything',
+        title: 'Patreon Creativity Over Everything',
         description: "Patreon is a membership platform that makes it easy for artists and creators to get paid. Patreon's brand campaign emphasizing the system for creative people is broken, to ditch the old ways and let fans give creators the freedom and stability they need to do their best creative work. Collaborated alongside brand team from design to launch during a <strong>tight 3-week turnaround, building responsible landing page with React, Parcel.js</strong>.",
         cta: 'Visit site',
         ctaLink: 'https://www.creativityovereverything.com/',
@@ -45,7 +55,7 @@ export const PROJECTS = [
         alt: 'Screenshot of Patreon Product webpage',
     },
     {
-        title: 'Work In Progress',
+        title: ' Dropbox Work In Progress',
         description: '<strong>Webby Award winning Dropbox Work in Progress Blogs</strong>, designed to communicate company updates and perspective. <strong>Worked closely with shareholders to launch under two month window with rebrand and migration</strong>. Built with Javascript, HTML5/CSS3, AEM.',
         cta: 'Visit site',
         ctaLink: 'https://blog.dropbox.com/',
@@ -75,12 +85,14 @@ export const PROFILE = {
             <Text>Website built with React, Parcel, and Vercel. Set in  Canela and GT America.</Text>
 
             <Eyebrow>Recognition</Eyebrow>
-            <Link href="https://winners.webbyawards.com/2019/websites/general-websites/business-blogwebsite/91997/work-in-progress">
-                The Webby Awards | People's Voice Winner; Dropbox Work in Progress Blogs
-            </Link>
+            FullStory Hackathon | Customer Love & Best In Show; Augmenting DXI
             <br />
             <Link href="https://winners.webbyawards.com/2019/websites/general-websites/community/83631/patreon">
                 The Webby Awards | Webby Winner; Patreon Community
+            </Link>
+            <br/>
+            <Link href="https://winners.webbyawards.com/2019/websites/general-websites/business-blogwebsite/91997/work-in-progress">
+                The Webby Awards | People's Voice Winner; Dropbox Work in Progress Blogs
             </Link>
         </>
     ),
