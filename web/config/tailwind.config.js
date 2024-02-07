7e960dfea50b7dd64b0ece0c3faee1a6ef68401d
Lisa Yip <imlisayip@gmail.com>
2024-02-07 14:39:26 -0800

    initial commit

---
+++ web/config/tailwind.config.js
@@ -0,0 +1,19 @@
+/** @type {import('tailwindcss').Config} */
+module.exports = {
+  content: ['src/**/*.{js,jsx,ts,tsx}'],
+  theme: {
+    extend: {
+      colors: {
+        cream: '#EFE6D7',
+        eyebrow: '#564A3E',
+        heading: '#222222',
+      },
+    },
+    fontFamily: {
+      body: ['gt-america-light', 'sans-serif'],
+      eyebrow: ['gt-america-medium', 'sans-serif'],
+      heading: ['canela-thin-web', 'serif'],
+    },
+  },
+  plugins: [],
+}
