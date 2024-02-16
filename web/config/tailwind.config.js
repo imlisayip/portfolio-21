65ab812c9951f38c8879fddd703d561cbac2a0ed
Lisa Yip <imlisayip@gmail.com>
2024-02-16 12:03:40 -0800

    ✨ Assets

--- web/config/tailwind.config.js
+++ web/config/tailwind.config.js
@@ -2,17 +2,93 @@
 module.exports = {
   content: ['src/**/*.{js,jsx,ts,tsx}'],
   theme: {
+    fontSize: {
+      sm: [
+        '0.78rem',
+        {
+          // eyebrow
+          lineHeight: '0.89rem',
+          letterSpacing: '2px',
+        },
+      ],
+      base: [
+        '0.9rem',
+        {
+          lineHeight: '1.33333rem',
+        },
+      ],
+
+      nav: '28px', // nav
+      lgm: [
+        '2.11rem',
+        {
+          // titles mobile
+          lineHeight: '1.96rem',
+        },
+      ],
+      lgt: [
+        '2.33rem',
+        {
+          // titles tablet
+          lineHeight: '2.04rem',
+        },
+      ],
+      lgd: [
+        '2.33rem',
+        {
+          // titles desktop
+          lineHeight: '2.23rem',
+        },
+      ],
+      xlm: [
+        '2.22rem',
+        {
+          // headings mobile
+          lineHeight: '2.67rem',
+        },
+      ],
+      xlt: [
+        '3.55556rem',
+        {
+          // headings tablet
+          lineHeight: '4.44rem',
+        },
+      ],
+      xld: [
+        '3.35rem',
+        {
+          // headings desktop
+          lineHeight: '4.4rem',
+        },
+      ],
+      name: [
+        '3.38889rem',
+        {
+          lineHeight: '4.05556rem',
+        },
+      ],
+    },
+    maxWidth: {
+      '3/4': '75%',
+    },
+    screens: {
+      sm: '480px',
+      md: '767px',
+      lg: '1023px',
+      xl: '1279px',
+      '2xl': '1535px',
+    },
     extend: {
       colors: {
         cream: '#EFE6D7',
-        eyebrow: '#564A3E',
+        highlight: '#564A3E',
         heading: '#222222',
         black: '#212121',
       },
     },
     fontFamily: {
       body: ['gt-america-light', 'sans-serif'],
-      eyebrow: ['gt-america-medium', 'sans-serif'],
+      highlight: ['gt-america-medium', 'sans-serif'],
       heading: ['canela-thin-web', 'serif'],
     },
   },
---
+++ web/public/files/data.tsx
@@ -0,0 +1,133 @@
+import COE from '../images/creativity-over-everything.png'
+import Transparency from '../images/dropbox-transparency.png'
+import HeatmapsExplore from '../images/fullstory-heatmaps.png'
+import JourneyMaps from '../images/fullstory-journey-maps.png'
+import Product from '../images/patreon-product.png'
+import SalesforceDreamforce from '../images/salesforce-dreamforce.png'
+import LisaYip from '../images/sf-trolley.png'
+import WorkInProgress from '../images/work-in-progress.png'
+
+// import RESUMEPATH from './YIP_Resume_24.pdf'
+
+export const HEADER = {
+  eyebrow: 'hello',
+  intro:
+    'Lisa Yip is a front end engineer, eager to embrace new opportunities.',
+}
+export const PROJECTS = [
+  {
+    title: 'Dreamforce 2023',
+    description:
+      "At Salesforce's Dreamforce 2023 conference, the focus lies in AI, Data, and CRM fusion, offering insights into AI-driven innovation for faster data-based decisions. Responsibilities included <strong>leading website enhancement, assuming leadership following a colleague's departure, supporting dynamic client needs, resolving legacy code complexities, and managing dynamic web elements and content</strong> utilizing HTML, SCSS, JavaScript, and Adobe Experience Manager (AEM).",
+    cta: 'Visit site',
+    ctaLink: 'https://www.dreamforce.com/',
+    image: SalesforceDreamforce,
+    alt: 'Screenshot of 2023 Salesforce Dreamforce Home page',
+    direction: 'right',
+  },
+  {
+    title: 'FullStory Heatmaps',
+    description:
+      'FullStory is a web-based digital intelligence system that helps optimize the client experience. Heatmaps provide cues for how users <strong>explore and navigate your website via visual, aggregate data</strong>. Worked closely with team to <strong>build ground work connecting APIs and visual pieces together using React, TypeScript</strong>, and Go.',
+    cta: 'Visit product page',
+    ctaLink: 'https://www.fullstory.com/platform/heatmaps/',
+    image: HeatmapsExplore,
+    alt: 'Screenshot of FullStory Heatmaps Product',
+  },
+  {
+    title: 'FullStory Journey Maps',
+    description:
+      'FullStory is a web-based digital intelligence system that helps optimize the client experience. Journey maps is a <strong>highly anticipated analysis for visualizing user paths</strong>. Worked closely with team to <strong>build and connect UI pieces together using React, TypeScript</strong>, and Go.',
+    cta: 'Visit product page',
+    ctaLink: 'https://www.fullstory.com/platform/journey-maps/',
+    image: JourneyMaps,
+    alt: 'Screenshot of FullStory Journey Maps Product',
+    direction: 'right',
+  },
+  {
+    title: 'Patreon Creativity Over Everything',
+    description:
+      'Patreon, a membership platform simplifying artist and creator payments, launched a brand campaign urging creative individuals to break free from outdated systems and empower fans to provide the stability needed for their best work. During a brief <strong>3-week timeline</strong>, collaborated with the <strong>brand team on design-to-launch efforts, building a responsive landing page using React and Parcel.js.</strong>',
+    cta: 'Visit site',
+    ctaLink: 'https://www.creativityovereverything.com/',
+    image: COE,
+    alt: 'Screenshot of Creativity over Everything webpage',
+  },
+  {
+    title: 'Patreon Product',
+    description:
+      'Patreon is a membership platform that makes it easy for artists and creators to get paid. Worked with the team to <strong>rebrand Patreon App Directory to add value to patrons with tools, benefits, and perks</strong>. Built website using React, Python.',
+    cta: 'Visit site',
+    ctaLink: 'https://www.patreon.com/product/premium',
+    image: Product,
+    alt: 'Screenshot of Patreon Product webpage',
+    direction: 'right',
+  },
+  {
+    title: ' Dropbox Work In Progress',
+    description:
+      '<strong>Webby Award winning Dropbox Work in Progress Blogs</strong>, designed to communicate company updates and perspective. <strong>Worked closely with shareholders to launch under two month window with rebrand and migration</strong>. Built with Javascript, HTML5/CSS3, AEM.',
+    cta: 'Visit site',
+    ctaLink: 'https://blog.dropbox.com/',
+    image: WorkInProgress,
+    alt: 'Screenshot of Dropbox blogs webpage',
+  },
+  {
+    title: 'Dropbox Transparency',
+    description:
+      'Dropbox is a modern workspace designed to reduce busywork-so you can focus on the things that matter. Site focused on being <strong>transparent about how government requests for user data is handled</strong>. Project built using <strong>D3.js, HTML5/CSS3 and AEM.</strong>',
+    cta: 'Visit site',
+    ctaLink: 'https://www.dropbox.com/transparency',
+    image: Transparency,
+    alt: 'Screenshot of Dropbox transparency webpage',
+    direction: 'right',
+  },
+]
+
+export const PROFILE = {
+  title: 'Lisa Yip',
+  description: [
+    `Hello! Thanks for dropping by. I'm a Front End Engineer actively on the
+        lookout for new and exciting opportunities. I enjoy creating meaningful
+        work that packs communication within a snapshot.`,
+    `Previously, I've had the privilege of crafting digital brand experiences
+        at FullStory, Patreon, and Dropbox.`,
+    `My experience includes helping organizations establish the building
+        blocks for a robust design system. I'm passionate about the creative
+        process, and I believe that empathy, innovation, and teamwork are key
+        ingredients in achieving the perfect balance between functionality and
+        aesthetics.`,
+    `I'm a perpetual learner, always seeking new knowledge and experiences.
+        In my free time, I give back by teaching yoga to the community and
+        indulging in art or dance classes. I also find myself completely
+        engrossed when volunteering at endurance events, losing track of time in
+        the pages of a good book, or exploring captivating new places.`,
+    `Feel free to reach out — I'm more than happy to have a chat, whether
+        it's about work or leisure!`,
+    `Website built with Typescript, Redwood.JS, and Tailwind. Set in
+        Canela and GT America.`,
+  ],
+  accomplishments: [
+    { event: 'Yoga Alliance | Registered Yoga Teacher 200', cta: '' },
+    { event: 'CCACA Student Exhibition', cta: '' },
+    {
+      event:
+        'FullStory Hackathon | Customer Love & Best In Show; Augmenting DXI',
+      cta: '',
+    },
+    {
+      event: `The Webby Awards | Webby Winner; Patreon Community`,
+      cta: 'https://winners.webbyawards.com/2019/websites/general-websites/community/83631/patreon',
+    },
+    {
+      event: `The Webby Awards | People's Voice Winner; Dropbox Work in Progress Blogs`,
+      cta: 'https://winners.webbyawards.com/2019/websites/general-websites/business-blogwebsite/91997/work-in-progress',
+    },
+  ],
+  image: LisaYip,
+  alt: 'A woman stands on a cable car, smiling, wearing a hat, sunglasses, maroon top, and mustard pants, with city buildings in the background.',
+  type: 'about',
+}
+export const LINKEDIN = 'https://www.linkedin.com/in/imlisayip/'
+export const GITHUB = 'https://github.com/imlisayip'
+export const RESUME = 'RESUMEPATH'
