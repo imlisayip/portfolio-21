ede247dbb0b01247f18d587b9a04e34a91bd6b64
Lisa Yip <imlisayip@gmail.com>
2024-02-22 13:23:58 -0800

    Add spacing to about page

--- web/src/pages/AboutPage/AboutPage.tsx
+++ web/src/pages/AboutPage/AboutPage.tsx
@@ -6,7 +6,7 @@ import { PROFILE } from '../../../public/files/data'
 
 const AboutPage = () => {
   return (
-    <>
+    <div className="my-10 sm:mb-40 md:mb-24">
       <Metadata title="About" description="About page" />
 
       {AboutBioCard({
@@ -16,7 +16,7 @@ const AboutPage = () => {
         alt: PROFILE.alt,
         accomplishments: PROFILE.accomplishments,
       })}
-    </>
+    </div>
   )
 }
 
